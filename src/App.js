3a8605c6c6f427b039a296cd578b5eaa0f65cae8
anson.chen <ab67325@gmail.com>
2024-10-31 17:17:52 +0800

    init commit

---
+++ src/App.js
@@ -0,0 +1,37 @@
+// app.js
+import React, { useEffect, useState } from 'react';
+import './App.css';
+
+function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    const apiUrl = process.env.REACT_APP_API_URL;
+
+    fetch(`${apiUrl}/`)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.text();
+      })
+      .then(data => {
+        setMessage(data);
+      })
+      .catch(error => {
+        console.error('There has been a problem with your fetch operation:', error);
+      });
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to Planora Web!</h1>
+        <p>This is a simple React app running in Docker.</p>
+        <p>{message}</p> {}
+      </header>
+    </div>
+  );
+}
+
+export default App;
